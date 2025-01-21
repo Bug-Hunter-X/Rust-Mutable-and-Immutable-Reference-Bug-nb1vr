2ac6f0a75eb2c19e4a9d66b9e3a40a90468e7655
BugHunter-X <askzenai@gmail.com>
2025-01-21 15:34:06 +0100

    Add bugSolution.rs file

---
+++ bugSolution.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let mut x = 5;
+    {
+        let y = &mut x; // y is a mutable reference to x
+        *y += 1; // Modify x through y
+    }
+    let z = &x; // z is an immutable reference to x
+
+    println!("x = {}", x); // x is now 6
+    println!("z = {}", *z); // This will correctly print 6 now because the scope is well-managed
+} 
